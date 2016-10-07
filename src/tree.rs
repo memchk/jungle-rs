974a2ccfb603cda67f048b0f558da8a83fa6d8d3
Carson Page <pagem.carson@gmail.com>
2016-10-07 10:16:27 -0500

    Restarted after looking at some previous art, added semi-safe abstractions

--- src/tree.rs
+++ src/tree.rs
@@ -1,70 +1,35 @@
 use generic_array::*;
-use std::ops::Deref;
-use std::ptr;
 
-struct NodePointer<T,N>(*mut Node<T,N>) where N: ArrayLength<NodePointer<T, N>>;
-impl<T,N> Copy for NodePointer<T,N> where N: ArrayLength<NodePointer<T, N>> {}
-impl<T,N> Clone for NodePointer<T,N> where N: ArrayLength<NodePointer<T, N>> { fn clone(&self) -> NodePointer<T,N> { *self } }
-
-impl<T,N> Default for NodePointer<T,N> 
-	where N: ArrayLength<NodePointer<T, N>> {
-	fn default() -> Self {
-		NodePointer(ptr::null_mut())
-	}
-}
-
-impl<T,N> Deref for NodePointer<T,N>
-	where N: ArrayLength<NodePointer<T, N>> {
-	type Target = Node<T,N>;
-
-	fn deref(&self) -> &Node<T,N> {
-		unsafe { &*self.0 }
-	}
-}
-
-struct Node<T,N: ArrayLength<NodePointer<T,N>>> {
+struct Node<T, N: ArrayLength<NodeRef<T,N>>> {
 	value: T,
-	parent: NodePointer<T,N>,
-	children: GenericArray<NodePointer<T,N>,N>
+	parent: NodeParentRef<T,N>,
+	children: GenericArray<NodeRef<T, N>, N>
 }
 
-impl<T,N> Node<T,N>
-	where N: ArrayLength<NodePointer<T, N>> {
-    fn new(value: T) -> Self{
-    	Node {
-    		value: value,
-    		parent: NodePointer::default(),
-    		children: GenericArray::new()
-    	}
-    }
+struct NodeRef<T, N: ArrayLength<NodeRef<T,N>>> {
+	ptr: Option<Box<Node<T,N>>>
 }
 
-struct Tree<T,N: ArrayLength<NodePointer<T,N>>>(NodePointer<T,N>);
-
-impl<T,N> Tree<T,N>
-	where N: ArrayLength<NodePointer<T, N>> {
-
-	fn new(value: T) -> Self {
-	    Tree(NodePointer(Box::into_raw(Box::new(Node::new(value)))))
-	}
-
-	fn parent(&mut self) -> bool{
-		let parent = self.0.parent;
-		if parent.0 == ptr::null_mut() {
-			false
-		} else {
-		    self.0 = parent;
-		    true
+impl<T, N: ArrayLength<NodeRef<T,N>>> Default for NodeRef<T,N> {
+	fn default() -> Self {
+		NodeRef {
+			ptr: None
 		}
 	}
+}
 
-	fn child(&mut self, child: usize) -> bool {
-		let child = self.0.children[child];
-		if child.0 == ptr::null_mut() {
-			false
-		} else {
-			self.0 = child;
-			true
+struct NodeParentRef<T, N: ArrayLength<NodeRef<T,N>>> {
+	ptr: Option<*mut Node<T,N>>
+}
+
+impl<T, N: ArrayLength<NodeRef<T,N>>> Default for NodeParentRef<T,N> {
+	fn default() -> Self {
+		NodeParentRef {
+			ptr: None
 		}
 	}
+}
+
+struct Tree<T, N: ArrayLength<NodeRef<T,N>>> {
+	root: NodeRef<T,N>
 }
