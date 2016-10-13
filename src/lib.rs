c9ad4010396fed0a428cff6acfa06c87ea06f1fb
Carson Page <pagem.carson@gmail.com>
2016-10-13 12:02:32 -0500

    Added some simple tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
+#![feature(test)]
 extern crate generic_array;
+extern crate test;
+pub use tree::Tree;
+pub use generic_array::typenum;
 
 mod tree;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
--- src/tree.rs
+++ src/tree.rs
@@ -1,8 +1,12 @@
-use generic_array::*;
-use std::ops::Deref;
-use std::ptr;
+use generic_array::{GenericArray, ArrayLength};
+pub use generic_array::typenum;
 
-struct Node<T, N: ArrayLength<NodeRef<T,N>>> {
+
+use std::ptr;
+use std::fmt;
+use std::mem;
+
+pub struct Node<T, N: ArrayLength<NodeRef<T,N>>> {
 	value: T,
 	parent: NodeRef<T,N> ,
 	children: GenericArray<NodeRef<T, N>, N>
@@ -18,7 +22,7 @@ impl<T, N: ArrayLength<NodeRef<T,N>>> Node<T,N> {
 	}
 }
 
-struct NodeRef<T, N: ArrayLength<NodeRef<T,N>>> {
+pub struct NodeRef<T, N: ArrayLength<NodeRef<T,N>>> {
 	ptr: *mut Node<T,N>
 }
 
@@ -39,12 +43,38 @@ impl<T, N: ArrayLength<NodeRef<T,N>>> NodeRef<T,N> {
 	}
 }
 
-struct Tree<T, N: ArrayLength<NodeRef<T,N>>> {
+pub struct Tree<T, N: ArrayLength<NodeRef<T,N>>> {
 	root: NodeRef<T,N>
 }
 
-impl<T,N: ArrayLength<NodeRef<T,N>>> Tree<T,N> {
-	fn new(value: T) -> Self {
+impl<T, N: ArrayLength<NodeRef<T,N>>> Drop for Tree<T,N> {
+	fn drop(&mut self) {
+		// Traverse to root node
+		while self.parent() {}
+		
+		let mut stk = Vec::new();
+		stk.push(self.get_ref());
+		while !stk.is_empty() {
+			
+			//let children = unsafe {(*stk.pop().unwrap().ptr).children.iter()};
+			let top = stk.pop().unwrap();
+			let children = unsafe {(*top.ptr).children.iter()};
+
+			for child in children {
+				if !child.is_null() {
+					stk.push(*child);
+				}
+			}
+
+			unsafe {
+				Box::from_raw(top.ptr);
+			}
+		}
+	}
+}
+
+impl<T, N: ArrayLength<NodeRef<T,N>>> Tree<T,N> {
+	pub fn new(value: T) -> Self {
 		Tree {
 			root: NodeRef {
 				ptr: Box::into_raw(Box::new(Node::new(value)))
@@ -52,7 +82,7 @@ impl<T,N: ArrayLength<NodeRef<T,N>>> Tree<T,N> {
 		}
 	}
 
-	fn parent(&mut self) -> bool {
+	pub fn parent(&mut self) -> bool {
 		let parent = unsafe {(*self.root.ptr).parent};
 		if parent.is_null() {
 			false
@@ -62,8 +92,11 @@ impl<T,N: ArrayLength<NodeRef<T,N>>> Tree<T,N> {
 		}
 	}
 
-	fn child(&mut self, index: usize) -> bool {
-		let child = unsafe {(*self.root.ptr).children[index]};
+	pub fn child(&mut self, index: usize) -> bool {
+		if index >= N::to_usize() {
+			return false;
+		}
+		let child = unsafe {*(*self.root.ptr).children.get_unchecked(index)};
 		if child.is_null() {
 			false
 		} else {
@@ -72,7 +105,7 @@ impl<T,N: ArrayLength<NodeRef<T,N>>> Tree<T,N> {
 		}
 	}
 
-	fn take(&mut self) -> Tree<T,N> {
+	pub fn take(&mut self) -> Tree<T,N> {
 		let rtn = Tree {
 			root: self.root
 		};
@@ -84,4 +117,91 @@ impl<T,N: ArrayLength<NodeRef<T,N>>> Tree<T,N> {
 		unsafe {(*rtn.root.ptr).parent = NodeRef::default()};
 		rtn
 	}
+
+	pub fn attach(&mut self,tree: Tree<T,N>, index: usize) -> bool {
+		if index >= N::to_usize() {
+			return false;
+		}
+		let child = unsafe {*(*self.root.ptr).children.get_unchecked(index)};
+		if child.is_null() {
+			unsafe {
+				(*tree.root.ptr).parent = self.root;
+				(*self.root.ptr).children[index] = tree.root;
+			};
+			mem::forget(tree);
+			true
+		} else {
+			false
+		}
+	}
+
+	fn get_ref(&self) -> NodeRef<T,N> {
+		self.root
+	}
+
+	pub fn value(&self) -> &T {
+		unsafe {&(*self.root.ptr).value}
+	}
+
+	pub fn value_mut(&self) -> &mut T {
+		unsafe {&mut (*self.root.ptr).value}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use tree::Tree;
+    use tree::typenum::*;
+    use test::Bencher;
+    #[test]
+    fn create_tree() {
+    	let t : Tree<u32,U2> = Tree::new(5);
+        assert_eq!(&5, t.value());
+    }
+
+
+    #[test]
+    fn check_attach() {
+    	let mut t : Tree<u32, U2> = Tree::new(0);
+    	t.attach(Tree::new(7),0);
+    	t.attach(Tree::new(15),1);
+    	let x = *{
+    		t.child(0);
+    		t.value()
+    	};
+    	assert_eq!(7, x);
+    	t.parent();
+    	let x = *{
+    		t.child(1);
+    		t.value()
+    	};
+    	assert_eq!(15, x);
+    }
+
+    #[test]
+    fn large_listtree() {
+    	let mut t : Tree<u32, U1> = Tree::new(0);
+    	for i in 0..1_000_000 {
+    		t.attach(Tree::new(i),0);
+    		t.child(0);
+    		t.value();
+    	}
+    }
+
+    #[bench]
+    fn test_alloc_attach(b: &mut Bencher) {
+   		let mut t : Tree<u32, U1> = Tree::new(0);
+   		b.iter(|| {
+   			t.attach(Tree::new(0),0);
+   			t.child(0);
+   		})
+    }
+    #[bench]
+    fn test_alloc_attach_unsafe(b: &mut Bencher) {
+   		let mut t : Tree<u32, U1> = Tree::new(0);
+   		b.iter(|| {
+   			t.attach(Tree::new(0),0);
+   			unsafe { t.child_unchecked(0) };
+   		})
+    }
 }
